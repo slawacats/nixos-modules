33f6a1dcae7861ceab77b1f4022c35c1e7efcb8e
slawacats <slawacats@gmail.com>
2026-09-07 15:44:48 +0300

    home.nix

---
+++ modules/home.nix
@@ -0,0 +1,52 @@
+
+
+{
+  home.username = "nd1";
+  home.homeDirectory = "/home/nd1";
+
+  home.packages = with pkgs; [
+    ncmpcpp
+    mpc
+    euphonica
+
+    # (rust-bin.stable.latest.default.override {
+    #   extensions = [ "rust-src" ];
+    #   targets = [ "x86_64-unknown-linux-musl" ];
+    # })
+  ];
+
+  services.mpd = {
+    enable = true;
+    musicDirectory = "${config.home.homeDirectory}/Music";
+    extraConfig = ''
+      audio_output {
+        type    "pipewire"
+        name    "PipeWire Sound Server"
+      }
+      auto_update "yes"
+    '';
+  };
+
+  programs.ncmpcpp = {
+    enable = true;
+    settings = {
+      mpd_host = "127.0.0.1";
+      mpd_port = "6600";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings.user.name  = "slawacats";
+    settings.user.email = "slawacats@gmail.com";
+    settings = {
+      alias = {
+        tree = "log --oneline --graph --decorate --all";
+      };
+    };
+  };
+
+  home.sessionVariables = {
+    PATH = "$HOME/go/bin:$PATH";
+  };
+}
