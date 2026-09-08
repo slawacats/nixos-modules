a398a97e9ef5e6b2ed7cbdcbb9ff4a7e771acce9
nd1 <slawacats@gmail.com>
2026-09-08 14:15:51 +0300

    :D

--- specific/nixos-nd1.nix
+++ specific/nixos-nd1.nix
@@ -1,9 +1,9 @@
-{config, pkgs, ...}: 
+{config, pkgs, lib, ...}: 
 
 {
   networking.hostName = "nixos-nd1";
   
-  environment.shellAliases = {
+  environment.shellAliases = lib.mkForce {
 	  reboot = "pkill brave; su -c \"sleep 0.5 && efibootmgr -n 2 && reboot\"";
 	  poweroff = "pkill brave; su -c \"sleep 0.5 && efibootmgr -n 2 && poweroff\"";
 	  swap = "su -c \"efibootmgr -n 3 && reboot\"";
